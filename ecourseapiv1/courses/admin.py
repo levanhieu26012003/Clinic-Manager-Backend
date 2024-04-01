e1fe86c59ea62eae5115ebdb08050c783140330c
Hieues <levanhieu26012003@gmail.com>
2024-04-01 23:05:55 +0700

    init project

---
+++ ecourseapiv1/courses/admin.py
@@ -0,0 +1,43 @@
+import cloudinary
+from django.contrib import admin
+from django.utils.html import mark_safe
+from courses.models import Category, Course, Lesson, User, Tag, Comment, Like
+from django import forms
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+
+class CourseForm(forms.ModelForm):
+    description = forms.CharField(widget=CKEditorUploadingWidget)
+
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+
+class MyCourseAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'created_date', 'updated_date', 'active']
+    search_fields = ['name', 'description']
+    list_filter = ['id', 'created_date', 'name']
+    readonly_fields = ['my_image']
+    form = CourseForm
+
+    def my_image(self, instance):
+        if instance:
+            if instance.image is cloudinary.CloudinaryResource:
+                return mark_safe(f"<img width='120' src='{instance.image.url}' />")
+
+            return mark_safe(f"<img width='120' src='/static/{instance.image.name}' />")
+
+    class Media:
+        css = {
+            'all': ('/static/css/style.css', )
+        }
+
+
+admin.site.register(Category)
+admin.site.register(Course, MyCourseAdmin)
+admin.site.register(Lesson)
+admin.site.register(User)
+admin.site.register(Tag)
+admin.site.register(Comment)
+admin.site.register(Like)
---
+++ ecourseapiv1/courses/models.py
@@ -0,0 +1,79 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from ckeditor.fields import RichTextField
+from cloudinary.models import CloudinaryField
+
+
+class User(AbstractUser):
+    avatar = CloudinaryField(null=True)
+
+
+class BaseModel(models.Model):
+    created_date = models.DateTimeField(auto_now_add=True, null=True)
+    updated_date = models.DateTimeField(auto_now=True, null=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(BaseModel):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(BaseModel):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ItemBase(BaseModel):
+    tags = models.ManyToManyField(Tag)
+
+    class Meta:
+        abstract = True
+
+
+class Course(ItemBase):
+    name = models.CharField(max_length=255)
+    description = RichTextField()
+    image = CloudinaryField(null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Lesson(ItemBase):
+    subject = models.CharField(max_length=255)
+    content = RichTextField()
+    image = CloudinaryField(null=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.subject
+
+
+class Interaction(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user_id} - {self.lesson_id}'
+
+    class Meta:
+        abstract = True
+
+
+class Comment(Interaction):
+    content = models.CharField(max_length=255)
+
+
+class Like(Interaction):
+
+    class Meta:
+        unique_together = ('user', 'lesson')
